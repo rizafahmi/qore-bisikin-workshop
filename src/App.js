719d13c9a1f4ba8a635dd7e12ce4d38deafd0ebe
Riza Fahmi <rizafahmi@gmail.com>
2021-02-02 12:19:35 +0700

    Hello world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Data from Qore will be here...
         </p>
         <a
           className="App-link"
