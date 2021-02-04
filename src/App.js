5db70a6057b665f83353852ed940cd68a8c1762d
Riza Fahmi <rizafahmi@gmail.com>
2021-02-04 17:43:09 +0700

    Finish connect with Qore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import qoreContext from "./qoreContext.js";
 
 function App() {
+  const feedbacks = qoreContext.view("allFeedback").useListRow();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Data from Qore will be here...
-        </p>
+        <pre>{JSON.stringify(feedbacks)}</pre>
         <a
           className="App-link"
           href="https://reactjs.org"
